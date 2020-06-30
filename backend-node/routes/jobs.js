b02c1c8043a22ca43d12dad8222ab5d1fb4b0e10
lalinda <nawinjith96@gmail.com>
2020-06-30 19:45:06 +0530

    Update jobs.js

--- backend-node/routes/jobs.js
+++ backend-node/routes/jobs.js
@@ -39,7 +39,24 @@ router.post("/addNewJob", function (req, res) {
              
         });
 
+/******************************************************** Get Ongoing Jobs *******************************************************/
 
+router.get("/getOngoingJobs", function (req, res, next) {
+    
+    Job.find( {$or: [ { jobStatus: "Queued" }, { jobStatus: "Started" },{ jobStatus: "Partially Completed" }, ] }, { jobNo: 1, jobType: 1, custId: 1, jobStatus: 1 } )
+        .select()
+        .exec() 
+        .then(data => {
+            console.log("Data Transfer Success..!")
+            //console.log(data);
+            res.json({ state: true, msg: "Data Transfer Success..!", data: data });
+        
+            })
+            .catch(error => {
+                console.log("Data Transfer Unsuccessfull..!")
+                res.json({ state: false, msg: "Data Transfer Unsuccessfull..!" });
+            })
+});
 
 
 
