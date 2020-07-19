ec16ff14359b398bbf452d11d87213d6846c0fb6
Sajith Thisera <thisera.sajith@gmail.com>
2020-07-19 13:26:27 +0530

    reservations model added

---
+++ backend-node/models/reservations.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const Schema = mongoose.schema;
+
+const reservationSchema = mongoose.Schema({
+    custID: { type: String, require: true },
+    daterequested: { type: String, require: true },
+    time: { type: String, require: true},
+    repairtype: { type: String, require: true },
+    problembrief: { type: String, require: true },
+    status: { type: String, require: true },
+});
+
+const reservations = module.exports = mongoose.model("reservations", reservationSchema );
