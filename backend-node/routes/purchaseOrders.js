844e17aba498ab5769e00b59aa959bdb4336d41b
Kavini <kavininanayakkara@gmail.com>
2020-07-25 15:09:29 +0530

    getPendingOrders route added

--- backend-node/routes/purchaseOrders.js
+++ backend-node/routes/purchaseOrders.js
@@ -31,4 +31,24 @@ router.post("/requestQuantity", function (req, res) {
 
 });
 
+
+/******************************************************** Get Pending Orders *******************************************************/
+router.get("/getPendingOrders", function (req, res) {
+    console.log("dds");
+    purchaseOrders.find( { status:0 }, { dateApplied: 1,itemid: 1, quantity: 1, supplierid: 1, dateReceived: 1, lastmodifiedby: 1, lastmodifiedon:1 } )
+      .select()
+      .exec()
+      .then(data => {
+        console.log("Data Transfer Success..!");
+        //console.log(data);
+        res.json({ state: true, msg: "Data Transfer Success..!", data: data });
+  
+      })
+      .catch(error => {
+        console.log("Data Transfer Unsuccessful..!");
+        res.json({ state: false, msg: "Data Transfer Unsuccessful..!" });
+      })
+  });
+
+
 module.exports = router; 
