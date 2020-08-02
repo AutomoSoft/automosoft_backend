89d58b857c65b94e784905759cbed5bc586284c4
Kavini <kavininanayakkara@gmail.com>
2020-08-02 10:56:16 +0530

    purchase-orders model added

--- backend-node/models/purchase-orders.js
+++ backend-node/models/purchase-orders.js
@@ -10,7 +10,6 @@ const purchaseOrdersSchema = mongoose.Schema({
     lastmodifiedby: { type: String, require: true },
     lastmodifiedon: { type: String, require: true },
     status: { type: String, require: true }
-
 });
 
 const purchaseOrders = module.exports = mongoose.model("purchaseOrders", purchaseOrdersSchema);
